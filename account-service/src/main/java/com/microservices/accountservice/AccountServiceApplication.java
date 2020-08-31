22327a0e52c37ea73fa1feeb201551d462cd9272
Chandrashekarrao <shekarrao166@gmail.com>
2020-08-31 19:37:10 +0530

    Added support for Zipkin distributed tracing

--- account-service/src/main/java/com/microservices/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/microservices/accountservice/AccountServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,6 +15,11 @@ public class AccountServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountServiceApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
--- customer-service/src/main/java/com/microservices/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/microservices/customerservice/CustomerServiceApplication.java
@@ -5,15 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableFeignClients("com.microservices.customerservice")
 @EnableDiscoveryClient
-//@EnableCircuitBreaker
+@EnableCircuitBreaker
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
